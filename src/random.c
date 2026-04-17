f29f5306250a1050c3b83e460c56388c472a757d
Jakub Rękas <74935888+JBRS307@users.noreply.github.com>
2026-04-17 19:50:12 +0200

    Parallel random for mac (#2)

--- src/random.c
+++ src/random.c
@@ -1,12 +1,16 @@
 #include "random.h"
 #include <stdint.h>
-
+#include <omp.h>
 
 #if defined (__APPLE__) && defined (__MACH__)
 
 #include <stdlib.h>
 
 void random_array(uint32_t* arr, size_t n_elems) {
+    size_t threads = omp_get_num_threads();
+    size_t chunk_size = n_elems / threads;
+
+    #pragma omp parallel for schedule(static, chunk_size)
     for (size_t i = 0; i < n_elems; i++) {
         arr[i] = arc4random();
     }
