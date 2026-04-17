43874583da074b182e590e63e050c5ae878e22e3
Jakub Rękas <kuba.rekas.307@gmail.com>
2026-04-17 20:22:47 +0200

    Running sort

--- src/main.c
+++ src/main.c
@@ -3,10 +3,33 @@
 #include <stdlib.h>
 #include <omp.h>
 
+#include "bucket.h"
+#include "random.h"
+
 #define N 10000
+
+void print_arr(uint32_t* array, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        printf("%u ", array[i]);
+    }
+    printf("\n");
+}
+
+size_t bucket_idx(uint32_t elem) {
+    return elem;
+}
 
 int main(void) {
     uint32_t array[N];
+    random_array(array, N);
+    print_arr(array, N);
+
+    Bucket* buckets = create_buckets(N, 10);
+    if (bucket_sort(array, N, buckets, N, bucket_idx) == FAILURE) {
+        exit(EXIT_FAILURE);
+    }
+
+    print_arr(array, N);
 
     return EXIT_SUCCESS;
 }
