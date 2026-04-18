62dfe7d5c283d10f56dabe12786075ba27db142a
eszlamczyk <ernest.szlamczyk@gmail.com>
2026-04-18 13:32:18 +0200

    format

--- src/random.c
+++ src/random.c
@@ -8,27 +8,27 @@
 #include <stdlib.h>
 
 void random_array(uint32_t *arr, size_t n_elems, uint32_t max) {
-  size_t threads = omp_get_num_threads();
-  size_t chunk_size = n_elems / threads;
+    size_t threads = omp_get_num_threads();
+    size_t chunk_size = n_elems / threads;
 
-#pragma omp parallel for schedule(static, chunk_size)
-  for (size_t i = 0; i < n_elems; i++) {
-    arr[i] = arc4random() % max;
-  }
+    #pragma omp parallel for schedule(static, chunk_size)
+    for (size_t i = 0; i < n_elems; i++) {
+        arr[i] = arc4random() % max;
+    }
 }
 
 #elif defined(__linux__)
 
 #include <stdlib.h>
 
-void random_array(uint32_t* arr, size_t n_elems, uint32_t max) {
-  size_t threads = omp_get_num_threads();
-  size_t chunk_size = n_elems / threads;
+void random_array(uint32_t *arr, size_t n_elems, uint32_t max) {
+    size_t threads = omp_get_num_threads();
+    size_t chunk_size = n_elems / threads;
 
-  #pragma omp parallel for schedule(static, chunk_size)
-  for (size_t i = 0; i < n_elems; i++) {
-    arr[i] = arc4random_uniform(max);
-  }
+    #pragma omp parallel for schedule(static, chunk_size)
+    for (size_t i = 0; i < n_elems; i++) {
+        arr[i] = arc4random_uniform(max);
+    }
 }
 
 #endif
